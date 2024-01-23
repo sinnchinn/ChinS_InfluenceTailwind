766ac14ff146ec640474bbb7579566be1468db16
sinnchinn <sinathachin@gmail.com>
2024-01-23 13:37:32 -0800

    starting tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+    "./pages/**/*.html"
+  ],
+  theme: {
+    colors: {
+      'custom-blue': 'rgba(181, 207, 231, 0.8)',
+      'custom-yellow': '#F1ECCE'
+    },
+    extend: {
+      fontFamily: {
+        elite: ['Special Elite', ...defaultTheme.fontFamily.sans]
+      },
+      backgroundImage: {
+        'bg-hero': 'url(../assets/bgg2.jpg)'
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
