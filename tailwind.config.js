8139e9c9cfa6b3e247434aae7e65b4af4d89714d
sinnchinn <sinathachin@gmail.com>
2024-01-23 22:34:56 -0800

    finishing

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
     colors: {
       'custom-blue': 'rgba(181, 207, 231, 0.8)',
       'custom-yellow': '#F4EFD1',
-      'custom-bg-card': '#E5EEF4'
+      'custom-bg-card': 'rgba(229, 238, 244, 0.8)'
     },
     extend: {
       fontFamily: {
@@ -21,6 +21,9 @@ module.exports = {
       },
       backgroundImage: {
         'bg-hero': 'url(../assets/background.jpg)',
+        'bg-png': 'url(../assets/mob1.png)',
+        'bg-png2': 'url(../assets/reigen1last.png)',
+        'bg-png3': 'url(../assets/dimple1.png)'
       }
     },
   },
